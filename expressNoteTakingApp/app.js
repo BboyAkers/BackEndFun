758e88a81d62eb75cf6c3c376a9e0adeb4e2b544
Austin Akers <austin.akers5@gmail.com>
2017-09-09 13:19:26 -0500

    added form and modified routes, cleared cookie

--- expressNoteTakingApp/app.js
+++ expressNoteTakingApp/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const name = req.cookies.username;
+    if(name){
+        res.render('index', { name });
+    }
+    else{
+        res.redirect('/hello');
+    }
+    
 });
 
 app.get('/test', (req, res) => {
@@ -17,12 +27,24 @@ app.get('/cards', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-    res.render('hello');
+    const name = req.cookies.username;
+    if(name){
+        res.redirect('/');
+    }
+    else {
+        res.render('hello');
+    }
+    
+});
+
+app.post('/goodbye', (req, res) => {
+        res.clearCookie('username');
+        res.redirect('/hello');    
 });
 
 app.post('/hello', (req, res) => {
-    console.dir(req);
-    res.render('hello');
+    res.cookie('username', req.body.username)
+    res.redirect('/');
 });
 
 app.listen(3000, () => {
