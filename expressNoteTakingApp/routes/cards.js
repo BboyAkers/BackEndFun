6a1f802b972e92341d549c318248c7d68a2b5816
Austin Akers <austin.akers5@gmail.com>
2017-09-15 21:11:35 -0500

    Randomized cards and modified variable and cookie access throughout the app

--- expressNoteTakingApp/routes/cards.js
+++ expressNoteTakingApp/routes/cards.js
@@ -3,9 +3,20 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+router.get('/', (req, res) => {
+    const numberOfCards = cards.length;
+    const flashcardId = Math.floor(Math.random() * numberOfCards);
+    res.redirect(`/cards/${flashcardId}`);
+});
+
 router.get('/:id', (req, res) => {
     const { side } = req.query;
     const { id } = req.params;
+
+    if(!side) {
+        res.redirect(`/cards/${id}?side=question`);
+    }
+    const name = req.cookies.username;
     const text = cards[id][side];
     const { hint } = cards[id];
 
