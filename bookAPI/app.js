127d6d9a2135cf74b6cc273937a7d802c3ee03d8
Austin Akers <austin.akers5@gmail.com>
2017-09-23 11:24:27 -0500

    created books api call and added error handling

--- bookAPI/app.js
+++ bookAPI/app.js
@@ -1,10 +1,31 @@
-var express = require('express');
+var express = require('express'),
+    mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
+var bookRouter = express.Router();
 
+bookRouter.route('/Books')
+    .get(function(req, res){
+        Book.find(function(err, books){
+            if(err){
+                res.status(500).send(err);
+            }
+            else{
+                res.json(books);
+            }
+        })
+        
+        res.json(responseJson);
+    });
+
+app.use('/api', bookRouter);
 
 app.get('/', function (req, res) {
    res.send('welcome to my API!');
