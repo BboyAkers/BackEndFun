51ef8cd09810457cc557e614841db69529817bfb
Austin Akers <austin.akers5@gmail.com>
2018-04-19 09:22:55 -0500

    added profile page and created a valid session for user login

--- userAuthExpress/routes/index.js
+++ userAuthExpress/routes/index.js
@@ -30,6 +30,23 @@ router.post('/login', function(req, res, next){
   }
 });
 
+//GET /profile
+router.get('/profile', function(req, res, next){
+  if(! req.session.userId){
+    var err = new Error('You are not authorized to view this page. Please log in');
+    err.status = 403;
+    return next(err)
+  }
+  User.findById(req.session.userId)
+    .exec(function (error, user) {
+      if (error) {
+        return next(error);
+      } else {
+        return res.render('profile', { title: 'Profile', name: user.name, favorite: user.favoriteBook });
+      }
+    });
+});
+
 //GET /register
 router.get('/register', function(req, res, next){
   return res.render('register', {title: 'Sign Up'});
