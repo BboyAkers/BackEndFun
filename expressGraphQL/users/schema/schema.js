b1bb73b8c6f1b11820f42237e18448948ef070b5
Austin Akers <austin.akers5@gmail.com>
2023-07-05 22:23:19 -0700

    created root query and imported graphql schema to server

--- expressGraphQL/users/schema/schema.js
+++ expressGraphQL/users/schema/schema.js
@@ -1,4 +1,9 @@
-import { GraphQLInt, GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLInt } from 'graphql';
+
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '43', firstName: 'Austin', age: 27 },
+]
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -8,3 +13,22 @@ const UserType = new GraphQLObjectType({
     age: { type: GraphQLInt }
   }
 });
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
+  }
+});
+
+const schema = new GraphQLSchema({
+  query: RootQuery
+});
+
+export default schema;
--- expressGraphQL/users/server.js
+++ expressGraphQL/users/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import expressGraphQL from 'express-graphql';
-
+import schema from './schema/schema.js';
 const app = express();
 
 const expressGraphQLInit = expressGraphQL.graphqlHTTP;
 
 app.use('/graphql', expressGraphQLInit({
+  schema,
   graphiql: true
 }));
 
