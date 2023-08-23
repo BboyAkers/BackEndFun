07e7d6fbb8d6cabcf691b42c1bb805d5fd035a05
Austin Akers <austin.akers5@gmail.com>
2023-08-22 20:59:37 -0700

    put fields in anon func to fix circular dependency problem and added new fields

--- expressGraphQL/users/schema/schema.js
+++ expressGraphQL/users/schema/schema.js
@@ -1,14 +1,36 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLInt } from 'graphql';
+import { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLInt, GraphQLList } from 'graphql';
 import axios from 'axios'
 
+const CompanyType = new GraphQLObjectType({
+  name: 'Company',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then(response => response.data);
+      }
+    }
+  })
+});
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
-  }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(res => res.data);
+      }
+    }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -19,6 +41,14 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id}`)
+          .then(response => response.data);
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
           .then(response => response.data);
       }
     }
